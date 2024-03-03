2b669d2908b13d13399d203a9cc1eb11d9271728
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2024-03-04 00:11:15 +0900

    fix: title representation

--- lib/fetchMetadata.ts
+++ lib/fetchMetadata.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import { load } from 'cheerio';
 
 export async function fetchMetadata (url: string) {
   try {
     const response = await axios.get(url);
     const html = response.data;
-    const $ = cheerio.load(html);
+    const $ = load(html);
 
-    const title = $('title').text();
+    const title = $('title').first().text();
     const description = $('meta[name="description"]').attr('content') || $('meta[property="og:description"]').attr('content');
     const ogImage = $('meta[property="og:image"]').attr('content');
 
